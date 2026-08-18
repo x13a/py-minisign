122dcfdf523e5126c33e3ce35cc86356425db6d1
lucky <>
2026-08-18 19:31:22 +0300

    add pub consts

--- minisign/__init__.py
+++ minisign/__init__.py
@@ -2,6 +2,7 @@
 Minisign
 """
 
+from .const import TRUSTED_COMMENT_MAX_LEN
 from .exceptions import (
     Error,
     ParseError,
@@ -9,10 +10,21 @@ from .exceptions import (
 )
 from .minisign import KeyPair
 from .public_key import PublicKey
+from .scrypt import (
+    MEMLIMIT,
+    MEMLIMIT_MAX,
+    OPSLIMIT,
+    OPSLIMIT_MAX,
+)
 from .secret_key import SecretKey
 from .signature import Signature
 
 __all__ = [
+    "MEMLIMIT",
+    "MEMLIMIT_MAX",
+    "OPSLIMIT",
+    "OPSLIMIT_MAX",
+    "TRUSTED_COMMENT_MAX_LEN",
     "Error",
     "KeyPair",
     "ParseError",
